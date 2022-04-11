b51e497b7157a7484f3eefcf30c0eee935c5da9b
Ilija Janchev <i.janchev@gmail.com>
2022-04-11 14:41:08 +0200

    class prefix in container App for preventing CSS conflicts

--- packages/container/src/components/App.js
+++ packages/container/src/components/App.js
@@ -1,14 +1,21 @@
+import { StylesProvider, createGenerateClassName } from "@material-ui/core";
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
 import Header from "./Header";
 import MarketingApp from "./MarketingApp";
 
+const generateClassName = createGenerateClassName({
+  productionPrefix: "con",
+});
+
 const App = () => {
   return (
-    <BrowserRouter>
-      <Header signedIn={false} onSignOut={() => console.log("sign out")} />
-      <MarketingApp />
-    </BrowserRouter>
+    <StylesProvider generateClassName={generateClassName}>
+      <BrowserRouter>
+        <Header signedIn={false} onSignOut={() => console.log("sign out")} />
+        <MarketingApp />
+      </BrowserRouter>
+    </StylesProvider>
   );
 };
 
